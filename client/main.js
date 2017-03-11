48aa43e34f89747818f12edbf8027cd56b74a42b
Louis-David Lacerte <ldlacerte@hotmail.com>
2017-03-11 01:08:19 -0500

    ajout des pistes cyclables

--- client/main.js
+++ client/main.js
@@ -32,21 +32,59 @@ Template.body.helpers({
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
       // Map initialization options
-      return {
-        center: new google.maps.LatLng(-37.8136, 144.9631),
-        zoom: 8
-      };
     }
   }
 });
 
-Template.body.onCreated(function() {
-  // We can use the `ready` callback to interact with the map API once the map is ready.
-  GoogleMaps.ready('exampleMap', function(map) {
-    // Add a marker to the map once it's ready
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
-    });
+Template.mapPostsList.rendered = function() {
+  var mapOptions = {
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+
+  map.setCenter(new google.maps.LatLng(45.514609, -73.636982));
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(45.514609, -73.636982),
+    title:'My Position',
+    icon:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
   });
-});
+  marker.setMap(map);
+
+  var bikeLayer = new google.maps.BicyclingLayer();
+  bikeLayer.setMap(map);
+
+  Session.set('map', true);
+
+  var directionsDisplay;
+  var directionsService = new google.maps.DirectionsService();
+
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+};
+
+// Template.body.onCreated(function() {
+//   // We can use the `ready` callback to interact with the map API once the map is ready.
+//   GoogleMaps.ready('ConvivMap', function(map) {
+//     // Add a marker to the map once it's ready
+//     // var marker = new google.maps.Marker({
+//     //   position: map.options.center,
+//     //   map: map.instance
+//     // });
+//     //var direction = new google.maps.
+//
+//     // var directionsService = new google.maps.DirectionsService;
+//     // var directionsDisplay = new google.maps.DirectionsRenderer({
+//     //   draggable: true,
+//     //   map: map,
+//     //   panel: document.getElementById('right-panel')
+//     // });
+//
+//     var marker = new google.maps.Marker({
+//       position: new google.maps.LatLng(45.514609, -73.636982),
+//       map: map.instance
+//     });
+//   });
+// });

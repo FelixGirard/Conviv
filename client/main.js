727334654d13d6d31d0fdbe7faddb752e89f4689
Louis-David Lacerte <ldlacerte@hotmail.com>
2017-03-11 01:31:25 -0500

    update des conflits

--- client/main.js
+++ client/main.js
@@ -21,6 +21,26 @@ import './main.html';
 //   },
 // });
 
+var mtlcenter = new google.maps.LatLng(45.514609, -73.636982);
+var dest =  new google.maps.LatLng(45.496270, -73.568704);
+
+function calcRoute() {
+  var request = {
+      origin: mtlcenter,
+      destination: dest,
+      // Note that Javascript allows us to access the constant
+      // using square brackets and a string value as its
+      // "property."
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  console.log("xd");
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
 if (Meteor.isClient) {
   Meteor.startup(function() {
     GoogleMaps.load();
@@ -37,17 +57,18 @@ Template.body.helpers({
 });
 
 Template.mapPostsList.rendered = function() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
   var mapOptions = {
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-
   map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
 
-  map.setCenter(new google.maps.LatLng(45.514609, -73.636982));
+  map.setCenter(mtlcenter);
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(45.514609, -73.636982),
+    position: mtlcenter,
     title:'My Position',
     icon:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
   });
@@ -56,14 +77,17 @@ Template.mapPostsList.rendered = function() {
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
 
+  directionsDisplay.setMap(map);
+
   Session.set('map', true);
-
-  var directionsDisplay;
-  var directionsService = new google.maps.DirectionsService();
-
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
 };
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    calcRoute();
+  },
+});
 
 // Template.body.onCreated(function() {
 //   // We can use the `ready` callback to interact with the map API once the map is ready.
